4bf63cf04b2af50de1a5dbe2151e85a37a714acf
violetzcm123 <2842134858@qq.com>
2026-01-17 14:08:38 +0800

    1

---
+++ Assets/Scripts/GameManager/GameNetworkManager.cs
@@ -0,0 +1,38 @@
+﻿using Mirror;
+
+namespace GameManager
+{
+    public class GameNetworkManager: NetworkRoomManager
+    {
+        public bool IsRoleTaken(int roleId, NetworkConnectionToClient except = null)
+        {
+            foreach (var slot in roomSlots)
+            {
+                if (slot == null) continue;
+                var rp = slot.GetComponent<MyRoomPlayer>();
+                if (rp != null && rp.roleId == roleId && slot.connectionToClient != except)
+                    return true;
+            }
+            return false;
+        }
+    }
+    public class MyRoomPlayer : NetworkRoomPlayer
+    {
+        [SyncVar(hook = nameof(OnRoleChanged))] public int roleId = -1;
+
+        [Command]
+        public void CmdSelectRole(int newRoleId)
+        {
+            var mgr = NetworkManager.singleton as GameNetworkManager;
+            if (mgr == null) return;
+            if (mgr.IsRoleTaken(newRoleId, connectionToClient)) return;
+
+            roleId = newRoleId;
+        }
+
+        void OnRoleChanged(int oldRole, int newRole)
+        {
+            // TODO: 更新房间 UI
+        }
+    }
+}
